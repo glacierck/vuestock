ea3365a6bcbd8cc68ca8b5108c44b6a727e82333
ck <494251050@qq.com>
2018-09-20 17:41:26 +0800

    Initial commit

---
+++ src/components/strategy.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <myheader :title="title"></myheader>
+  </div>
+</template>
+<script>
+import myheader from './header.vue'
+export default {
+  name: 'stategy',
+  data() {
+    return {
+      title: 'XX策略'
+    };
+  },
+  computed: {},
+  watch: {},
+  components: {
+    myheader
+  }
+};
+
+</script>
+<style>
+
+
+</style>
